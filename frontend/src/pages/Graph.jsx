e4ad1ece881a68200d86485499af8731133cae81
MITHRAN-BALACHANDER <mithranb15@gmail.com>
2025-04-01 09:28:05 +0530

    update

---
+++ frontend/src/pages/Graph.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Graph = () => {
+    return (
+        <div>
+            <h1>Graph Page</h1>
+            <p>This is where the graph will be displayed.</p>
+        
+        </div>
+    );
+};
+
+export default Graph;
---
+++ frontend/src/pages/LogsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LogsPage = () => {
+    return (
+        <div>
+            <h1>Logs Page</h1>
+            <p>This is where the logs will be displayed.</p>
+        </div>
+    );
+};
+
+export default LogsPage;
