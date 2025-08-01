c4cc6a7ad0098aaa742f9526ac9917e1fc5beed4
NISHAKAR T <nishakarnishakar06@gmail.com>
2025-08-01 20:54:21 +0530

    updated

--- backend/motion_control/urls.py
+++ backend/motion_control/urls.py
@@ -1,16 +1,7 @@
 from django.urls import path
-<<<<<<< Updated upstream
-from .views import IK2DView, MotionProfileView
+from .views import IK6DView, MotionProfileView
 
 urlpatterns = [
-    path('ik/', IK2DView.as_view(), name='ik-2d'),
+    path('ik/', IK6DView.as_view(), name='ik-6d'),
     path('<str:profile_name>/', MotionProfileView.as_view(), name='motion-profile'),
-=======
-from .views import run_motion_profile, move_robot
-
-
-urlpatterns = [
-    path('motion/<str:profile_type>/', run_motion_profile),
-    path('move_robot/', move_robot, name='move_robot'),
->>>>>>> Stashed changes
 ]
